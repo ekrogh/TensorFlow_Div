507b349c88a1dcfaeaa4de71de9afc9e9d30d139
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-03 01:44:53 +0200

    : --> ;

--- buildTensorflow_CPU.cmd
+++ buildTensorflow_CPU.cmd
@@ -1,14 +1,15 @@
-set PATH=D:/Program_Files/Python/311:%PATH%
-set PATH=D:/Program_Files/Python/311/Scripts:%PATH%
+set PATH=D:/Program_Files/Python/311;%PATH%
+set PATH=D:/Program_Files/Python/311/Scripts;%PATH%
 @REM set PYTHON_BIN_PATH=path/to/python_virtualenv/Scripts/python.exe 
 @REM set PYTHON_LIB_PATH=path/to/python virtualenv/lib/site-packages 
 @REM set PYTHON_DIRECTORY=path/to/python_virtualenv/Scripts 
 
+set BAZEL_WINSDK_FULL_VERSION=10.0.26100.0
 set BAZEL_SH=D:/Program_Files/msys64/usr/bin/bash.exe 
 set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools
 set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC 
 set Bazel_LLVM=D:/Program_Files/LLVM
-set PATH=D:/Program_Files/LLVM/bin:%PATH%
+set PATH=D:/Program_Files/LLVM/bin;%PATH%
 
 bazel clean 
 bazel clean --expunge  
