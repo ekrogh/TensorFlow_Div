778b03dce602cb0a020e2c601e52bfb86eda0dfc
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-03 01:15:46 +0200

    div

--- buildTensorflow_CPU.cmd
+++ buildTensorflow_CPU.cmd
@@ -1,6 +1,18 @@
+set PATH=D:/Program_Files/Python/311:%PATH%
+set PATH=D:/Program_Files/Python/311/Scripts:%PATH%
+@REM set PYTHON_BIN_PATH=path/to/python_virtualenv/Scripts/python.exe 
+@REM set PYTHON_LIB_PATH=path/to/python virtualenv/lib/site-packages 
+@REM set PYTHON_DIRECTORY=path/to/python_virtualenv/Scripts 
+
+set BAZEL_SH=D:/Program_Files/msys64/usr/bin/bash.exe 
+set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools
+set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC 
+set Bazel_LLVM=D:/Program_Files/LLVM
+set PATH=D:/Program_Files/LLVM/bin:%PATH%
+
 bazel clean 
 bazel clean --expunge  
 
 call configure.cmd
 
-bazel build --config=win_clang --extra_toolchains=@local_config_cc//:cc-toolchain-x64_windows-clang-cl --copt=-ID:\Program_Files\Microsoft_Visual_Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\include --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
+bazel build --copt=-I./include/vadefs.h --copt=-I./include/atomic --config=win_clang --extra_toolchains=@local_config_cc//:cc-toolchain-x64_windows-clang-cl --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
--- buildTensorflow_GPU.cmd
+++ buildTensorflow_GPU.cmd
@@ -1,7 +1,19 @@
+set PATH=D:/Program_Files/Python/311:%PATH%
+set PATH=D:/Program_Files/Python/311/Scripts:%PATH%
+@REM set PYTHON_BIN_PATH=path/to/python_virtualenv/Scripts/python.exe 
+@REM set PYTHON_LIB_PATH=path/to/python virtualenv/lib/site-packages 
+@REM set PYTHON_DIRECTORY=path/to/python_virtualenv/Scripts 
+
+set BAZEL_SH=D:/Program_Files/msys64/usr/bin/bash.exe 
+set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools
+set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC 
+set Bazel_LLVM=D:/Program_Files/LLVM
+set PATH=D:/Program_Files/LLVM/bin:%PATH%
+
 bazel clean 
 bazel clean --expunge  
 
-git checkout r2.10
+git checkout -f r2.10
 
 call configure.cmd
 
