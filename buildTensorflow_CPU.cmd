fe8b799e1e33bb7e5d032fe06364b62a35af88fc
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-02 22:38:24 +0200

    div

--- buildTensorflow_CPU.cmd
+++ buildTensorflow_CPU.cmd
@@ -3,4 +3,4 @@ bazel clean --expunge
 
 call configure.cmd
 
-bazel build --config=win_clang --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
+bazel build --config=win_clang --config=v2 --copt=-ID:\Program_Files\Microsoft_Visual_Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\include --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
