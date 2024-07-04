b70295d90fb2dfbf1d719c5471e43b3a2d15f83b
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-05 00:54:54 +0200

    @REM --> #

--- buildTensorflow_CPU.sh
+++ buildTensorflow_CPU.sh
@@ -29,10 +29,10 @@ export Bazel_LLVM=/d/Program_Files/LLVM
 
 
 bazel build --config=win_clang --config=v2 --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu
-@REM bazel build //tensorflow/tools/pip_package:build_pip_package --subcommands
+# bazel build //tensorflow/tools/pip_package:build_pip_package --subcommands
 
-@REM bazel aquery 'mnemonic("CppCompile", //tensorflow/tools/pip_package:build_pip_package)' --output=text
+# bazel aquery 'mnemonic("CppCompile", //tensorflow/tools/pip_package:build_pip_package)' --output=text
 
-@REM bazel build --config=opt --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu
+# bazel build --config=opt --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu
 
-@REM bazel build --copt=["-Iinclude"] --config=win_clang --extra_toolchains=@local_config_cc//:cc-toolchain-x64_windows-clang-cl --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
+# bazel build --copt=["-Iinclude"] --config=win_clang --extra_toolchains=@local_config_cc//:cc-toolchain-x64_windows-clang-cl --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu --define=no_tensorflow_py_deps=true --copt=-nvcc_options=disable-warnings
--- setPaths.sh
+++ setPaths.sh
@@ -1,1 +1,1 @@
-export "/d/Users/eigil/projects/machineLearning/TensorFlow_Div:$PATH"
+export PATH="/d/Users/eigil/projects/machineLearning/TensorFlow_Div:$PATH"
