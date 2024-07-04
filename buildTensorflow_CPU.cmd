b6d3fd31a4839f159337686f0eefca44eec378a6
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-04 18:10:11 +0200

    bazel build --config=win_clang --config=v2 --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu

--- buildTensorflow_CPU.cmd
+++ buildTensorflow_CPU.cmd
@@ -20,7 +20,7 @@ set Bazel_LLVM=D:/Program_Files/LLVM
 @REM set PATH=D:/Program_Files/LLVM/bin;%PATH%
 
 
-bazel build --config=win_clang --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu
+bazel build --config=win_clang --config=v2 --repo_env=TF_PYTHON_VERSION=3.11 //tensorflow/tools/pip_package:build_pip_package --repo_env=WHEEL_NAME=tensorflow_cpu
 @REM bazel build //tensorflow/tools/pip_package:build_pip_package --subcommands
 
 @REM bazel aquery 'mnemonic("CppCompile", //tensorflow/tools/pip_package:build_pip_package)' --output=text
