85d1139df43d47bda79465414ece92d68cc2cde0
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-07-04 16:33:21 +0200

    in all

--- buildTensorflow_CPU.cmd
+++ buildTensorflow_CPU.cmd
@@ -1,5 +1,5 @@
-set PATH=D:\Program_Files\Microsoft_Visual_Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64;D:\Program_Files\Microsoft_Visual_Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\include;%PATH%
-@REM set PATH=D:\Program_Files\Microsoft_Visual_Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64;%PATH%
+set PATH=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC/Tools/MSVC/14.40.33807/bin/Hostx64/x64;D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC/Tools/MSVC/14.40.33807/include;%PATH%
+@REM set PATH=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64;%PATH%
 set PATH=D:/Program_Files/Python/311;%PATH%
 set PATH=D:/Program_Files/Python/311/Scripts;%PATH%
 @REM set PYTHON_BIN_PATH=path/to/python_virtualenv/Scripts/python.exe 
@@ -7,13 +7,15 @@ set PATH=D:/Program_Files/Python/311/Scripts;%PATH%
 @REM set PYTHON_DIRECTORY=path/to/python_virtualenv/Scripts 
 
 set BAZEL_WINSDK_FULL_VERSION=10.0.19041.0
-SET INCLUDE=D:\Program_Files\Microsoft_Visual_Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\include;%INCLUDE%
+SET INCLUDE=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC/Tools/MSVC/14.40.33807/include;%INCLUDE%
 @REM SET INCLUDE=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133/include;%INCLUDE%
 @REM set BAZEL_WINSDK_FULL_VERSION=10.0.26100.0
 set BAZEL_SH=D:/Program_Files/msys64/usr/bin/bash.exe 
-set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools
-@REM set BAZEL_VC=D:\Program_Files\Microsoft_Visual_Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133
-set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools/VC 
+set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools
+set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2022/BuildTools/VC
+@REM set BAZEL_VS=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools
+@REM set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133
+@REM set BAZEL_VC=D:/Program_Files/Microsoft_Visual_Studio/2019/BuildTools/VC 
 @REM set Bazel_LLVM=D:/Program_Files/LLVM
 @REM set PATH=D:/Program_Files/LLVM/bin;%PATH%
 
